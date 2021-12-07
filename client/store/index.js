80006ae2532ec5f784666b9645ec59931e42de12
Juan Mateo <throwaway4classjmateo@gmail.com>
2021-12-07 17:53:16 -0500

    updated combineReducer

--- client/store/index.js
+++ client/store/index.js
@@ -3,8 +3,12 @@ import {createLogger} from 'redux-logger'
 import thunkMiddleware from 'redux-thunk'
 import {composeWithDevTools} from 'redux-devtools-extension'
 import auth from './auth'
+import produceReducer from './produce'
 
-const reducer = combineReducers({ auth })
+const reducer = combineReducers({
+  auth,
+  produce: produceReducer
+})
 const middleware = composeWithDevTools(
   applyMiddleware(thunkMiddleware, createLogger({collapsed: true}))
 )

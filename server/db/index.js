4353c16b505d1fe38ff2c428fbdefc2c7c4b0fe4
addislw <addislw@gmail.com>
2021-12-10 12:27:02 -0500

    Associations set between Order and Cart models

--- server/db/index.js
+++ server/db/index.js
@@ -4,13 +4,19 @@ const db = require("./db");
 
 const User = require("./models/User");
 const Grocery = require("./models/Grocery");
+const Cart = require("./models/Cart");
+const Order = require("./models/Order");
 
 //associations could go here!
+Order.belongsToMany(Grocery, { through: Cart });
+Grocery.belongsToMany(Order, { through: Cart });
 
 module.exports = {
   db,
   models: {
     User,
     Grocery,
+    Cart,
+    Order
   },
 };

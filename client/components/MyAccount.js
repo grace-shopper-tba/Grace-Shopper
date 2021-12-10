a1ed0b0dfa255391b871a79fbd4bdc746c8b11b5
Juan Mateo <throwaway4classjmateo@gmail.com>
2021-12-10 16:56:43 -0500

    started MyAccount; updated Home

---
+++ client/components/MyAccount.js
@@ -0,0 +1,15 @@
+import React from ' react'
+
+class MyAccount extends React.Component {
+  render() {
+    return (
+      <div>
+        <p>
+          Hello this is a placeholder
+        </p>
+      </div>
+    )
+  }
+}
+
+export default (MyAccount)

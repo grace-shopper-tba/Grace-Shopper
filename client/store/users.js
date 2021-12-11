5f2b6fa2baf37ac30cb3fd116e9fedd94ef01bd2
Chenel Morgan <chenel.morgan@gmail.com>
2021-12-10 21:30:30 -0500

    feat: authenticate allUsers route

--- client/store/users.js
+++ client/store/users.js
@@ -1,22 +1,27 @@
-import axios from 'axios';
+import axios from 'axios'
+const TOKEN = 'token'
 
-const SET_USERS = 'SET_USERS';
+const SET_USERS = 'SET_USERS'
 
 export const setUsers = (users) => {
   return {
     type: SET_USERS,
-    users
+    users,
   }
 }
 // Note: May need a new backend route for admins to see all user data, or to modify the existing one?
+
+//send the request only if the user is logged in
 export const fetchUsers = () => {
   return async (dispatch) => {
-    try {
-      const {data} = await axios.get('/api/users');
-      dispatch(setUsers(data));
-    }
-    catch (err) {
-      console.log(err)
+    const token = window.localStorage.getItem(TOKEN)
+    if (token) {
+      const { data } = await axios.get('/api/users', {
+        headers: {
+          authorization: token,
+        },
+      })
+      return dispatch(setUsers(data))
     }
   }
 }
@@ -24,8 +29,8 @@ export const fetchUsers = () => {
 export default function usersReducer(state = [], action) {
   switch (action.type) {
     case SET_USERS:
-      return action.users;
+      return action.users
     default:
-      return state;
+      return state
   }
 }

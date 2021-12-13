a7d1784864a4cddf6d052fec242973d5a23e0cdf
Juan Mateo <throwaway4classjmateo@gmail.com>
2021-12-13 14:42:07 -0500

    added add and remove placeholders to cart redux

--- client/store/cart.js
+++ client/store/cart.js
@@ -1,6 +1,10 @@
 import axios from 'axios';
-//Note: Will need seperate actions/thunks/cases for adding and removing items from cart
+// Note: Will need seperate actions/thunks/cases for adding and removing items from cart
 const SET_CART = 'SET_CART'
+const ADD_TO_CART = 'ADD_TO_CART'
+const REMOVE_FROM_CART = 'REMOVE_FROM_CART'
+
+// actions
 
 export const setCart = (cart) => {
   return {
@@ -8,6 +12,22 @@ export const setCart = (cart) => {
     cart
   }
 }
+
+export const _addToCart = (cart) => {
+  return {
+    type: ADD_TO_CART,
+    cart
+  }
+}
+
+export const _removeFromCart = (cart) => {
+  return {
+    type: REMOVE_FROM_CART,
+    cart
+  }
+}
+
+// thunks
 
 export const fetchCart = () => {
   return async (dispatch) => {
@@ -21,10 +41,41 @@ export const fetchCart = () => {
   }
 }
 
+export const addToCart = () => {
+  return async (dispatch) => {
+    try {
+      const {data} = await axios.put('route to update cart')
+      dispatch(_addToCart(data))
+    }
+    catch (err) {
+      console.log(err)
+    }
+  }
+}
+
+export const removeFromCart = () => {
+  return async (dispatch) => {
+    try {
+      const {data} = await axios.put('route to remove item from cart')
+      dispatch(_removeFromCart(data))
+    }
+    catch (err) {
+      console.log(err)
+    }
+  }
+}
+
+
+//reducer
+
 export default function cartReducer(state = [], action) {
   switch (action.type) {
     case SET_CART:
       return action.cart
+    case ADD_TO_CART:
+      return state
+    case REMOVE_FROM_CART:
+      return state
     default:
       return state
   }
